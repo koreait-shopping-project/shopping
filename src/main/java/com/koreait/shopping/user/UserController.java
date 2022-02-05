5d3b8c1187e3283564f885ecaf3b80dfee60ae7e
korait-shopping/Shopping <wogml3270@gmail.com>
2022-02-05 12:33:45 +0900

    login 백엔드 처리, login.css 살짝 변경

--- src/main/java/com/koreait/shopping/user/UserController.java
+++ src/main/java/com/koreait/shopping/user/UserController.java
@@ -1,19 +1,26 @@
 package com.koreait.shopping.user;
 
+import com.koreait.shopping.model.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired private UserService service;
+
     @GetMapping("/login")
     public void login(){}
+
+    @PostMapping("/login")
+    public String loginProc(UserEntity entity, RedirectAttributes reAttr){
+        int result = service.login(entity);
+        return "redirect:/board/main";
+    }
 
     @GetMapping("/join")
     public void join(){}
--- src/main/java/com/koreait/shopping/user/UserMapper.java
+++ src/main/java/com/koreait/shopping/user/UserMapper.java
@@ -1,7 +1,9 @@
 package com.koreait.shopping.user;
 
+import com.koreait.shopping.model.entity.UserEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
+    UserEntity selUser(UserEntity entity);
 }
