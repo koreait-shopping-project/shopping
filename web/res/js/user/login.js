ba630a3552027eeb7c97ae0bc34111535d82d0b9
ssaa889 <sar6747@naver.com>
2022-02-14 14:18:46 +0900

    소셜 로그인(2) 카카오 로그인, 구글 로그인 확인 완료

--- web/res/js/user/login.js
+++ web/res/js/user/login.js
@@ -119,10 +119,10 @@
 
     function onSignIn(googleUser) {
         const profile = googleUser.getBasicProfile();
-        const account_email = profile.getEmail();
-        const social = 'google';
+        const email = profile.getEmail();
+        const social = 'google'
         const param = {
-            'email' : account_email
+            'email' : email
         }
         // window.location.href="/user/join?email=" + email;
         // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -131,14 +131,13 @@
         myFetch.get('/user/selSocial', data => {
             switch (data.result) {
                 case 1:
-                    window.location.href = `/user/join?social=${social}&email=${account_email}`;
+                    window.location.href = `/user/join?social=${social}&email=${email}`;
                     break;
                 case 0:
                     window.location.href = '/user/main'
                     break;
             }
         }, param);
-
     }
 
     function onSignInFailure(t) {
@@ -163,5 +162,4 @@
             console.log('탈퇴');
         });
     }
-
 }
