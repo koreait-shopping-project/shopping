0e12291ccde8bf772695274869197320a833b5b6
koreait-shopping <85227527+cooo23@users.noreply.github.com>
2022-02-17 16:18:48 +0900

    ajax 통신(1)

---
+++ web/res/js/board/productdetail.js
@@ -0,0 +1,29 @@
+{
+    dataElem = document.querySelector('#data');
+    iboard = dataElem.dataset.iboard;
+
+    const selectElement = document.querySelector('#colorbox');
+
+    selectElement.addEventListener('change', (event) => {
+        const color = event.target.value;
+        // console.log(color);
+        const param = {
+            'iboard' : iboard,
+            'color' : color
+        }
+
+        myFetch.get('/board/size', data => {
+            switch (data.result) {
+                case 1:
+                    console.log('성공');
+                    break
+                case 0:
+                    console.log('실패');
+                    break
+            }
+        }, param);
+    });
+
+}
+
+// color 선택 시 color를 보내서
