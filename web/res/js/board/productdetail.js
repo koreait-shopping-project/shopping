b7e1f59d8b0b52b3a0ef26f34617704c713aa947
koreait-shopping <85227527+cooo23@users.noreply.github.com>
2022-02-18 10:13:59 +0900

    색상 선택시 사이즈 뜨도록 ajax완성

--- web/res/js/board/productdetail.js
+++ web/res/js/board/productdetail.js
@@ -2,9 +2,10 @@
     dataElem = document.querySelector('#data');
     iboard = dataElem.dataset.iboard;
 
-    const selectElement = document.querySelector('#colorbox');
+    const selectElem = document.querySelector('#colorbox');
+    const sizeElem = document.querySelector('#sizebox');
 
-    selectElement.addEventListener('change', (event) => {
+    selectElem.addEventListener('change', (event) => {
         const color = event.target.value;
         // console.log(color);
         const param = {
@@ -14,14 +15,13 @@
 
         myFetch.get('/board/size', data => {
             switch (data.result) {
-                case 1:
-                    console.log('성공');
-                    break
-                case 0:
-                    console.log('실패');
+                case data.result:
+                    sizeElem.options[1] = new Option('sm', `${data.result.sm}`);
+                    sizeElem.options[2] = new Option('md', `${data.result.md}`);
+                    sizeElem.options[3] = new Option('lg', `${data.result.lg}`);
+                    sizeElem.options[4] = new Option('xl', `${data.result.xl}`);
                     break
             }
         }, param);
     });
-
 }
