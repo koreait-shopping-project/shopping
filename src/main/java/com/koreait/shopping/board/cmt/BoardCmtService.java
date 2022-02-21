9fe169f83d18f4d40dc5e3a623f1329ef4efa294
cooo23 <85227527+cooo23@users.noreply.github.com>
2022-02-21 18:45:31 +0900

    detail cmt 작업(1)

--- src/main/java/com/koreait/shopping/board/cmt/BoardCmtService.java
+++ src/main/java/com/koreait/shopping/board/cmt/BoardCmtService.java
@@ -15,7 +15,7 @@ public class BoardCmtService {
     @Autowired private BoardCmtMapper mapper;
 
     public int insBoardCmt(BoardCmtEntity entity) { //iboard, ctnt
-        entity.setIuser(userUtils.getLoginUserPk()); //iboard, ctnt, iuser
+//        entity.setIuser(userUtils.getLoginUserPk()); //iboard, ctnt, iuser
         mapper.insBoardCmt(entity); //iboard, ctnt, iuser, (BoardCmtMapper - ins에서 useGeneratedKeys="true" keyProperty="icmt" 속성을 설정했기 때문에) icmt
         return entity.getIcmt();
     }
