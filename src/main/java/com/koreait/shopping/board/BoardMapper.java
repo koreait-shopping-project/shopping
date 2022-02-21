1c0aa0596922d00ba61fc35803e73fe8188357d9
koreait-shopping <icepri3535@gmail.com>
2022-02-21 17:59:03 +0900

    공지사항 프론트 페이징 빼고 완료, 검색 대충 완료

--- src/main/java/com/koreait/shopping/board/BoardMapper.java
+++ src/main/java/com/koreait/shopping/board/BoardMapper.java
@@ -4,6 +4,7 @@ import com.koreait.shopping.board.model.dto.BoardListDto;
 import com.koreait.shopping.board.model.dto.BoardProductDto;
 import com.koreait.shopping.board.model.entity.BoardListEntity;
 import com.koreait.shopping.board.model.entity.BoardPrevNextVo;
+import com.koreait.shopping.board.model.entity.BoardProductEntity;
 import com.koreait.shopping.board.model.vo.BoardListVo;
 import com.koreait.shopping.board.model.vo.BoardProductVo;
 import org.apache.ibatis.annotations.Mapper;
